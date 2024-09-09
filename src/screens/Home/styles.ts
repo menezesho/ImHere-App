46ab30415daa83cfa97fad50f4e339fdcc753ad3
Henrique Menezes <82477364+menezesho@users.noreply.github.com>
2024-09-09 10:41:27 -0300

    feat - início da implementação da home page
    - input e botão de inserção de participante
    - componente de participantes

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
         padding: 24,
     },
     eventName: {
-        color: '#FDFCFE',
+        color: '#FFF',
         fontSize: 24,
         fontWeight: 'bold',
         marginTop: 48,
@@ -15,5 +15,33 @@ export const styles = StyleSheet.create({
     eventDate: {
         color: '#6B6B6B',
         fontSize: 16,
+    },
+    input: {
+        flex: 1,
+        backgroundColor: '#1F1E25',
+        height: 56,
+        borderRadius: 4,
+        color: '#FFF',
+        padding: 16,
+        fontSize: 16,
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 24,
+    },
+    button: {
+        width: 56,
+        height: 56,
+        borderRadius: 4,
+        backgroundColor: '#31CF67',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    form: {
+        width: '100%',
+        flexDirection: 'row',
+        gap: 12,
+        marginTop: 36,
+        marginBottom: 42,
     }
 });
